6229365fbeb14383330719504636f558ddfbdea8
snatcher93 <snatcher93@hanmail.net>
2020-03-31 17:25:54 +0900

    oAuth 기본 구현 추가

--- src/main/java/org/eternity/book/springboot/config/jpa/JpaConfiguration.java
+++ src/main/java/org/eternity/book/springboot/config/jpa/JpaConfiguration.java
@@ -1,4 +1,4 @@
-package org.eternity.book.springboot.config;
+package org.eternity.book.springboot.config.jpa;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
