0b99f4b9ad590f89423f2f725b982f95f2d1dfb8
snatcher93 <snatcher93@hanmail.net>
2020-04-08 14:47:22 +0900

    Security Test 추가

--- src/test/java/org/eternity/book/springboot/web/IndexControllerUnitTest.java
+++ src/test/java/org/eternity/book/springboot/web/IndexControllerUnitTest.java
@@ -1,9 +1,15 @@
 package org.eternity.book.springboot.web;
 
+import org.eternity.book.springboot.config.oauth.SecurityConfig;
+import org.eternity.book.springboot.service.posts.PostsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,12 +18,19 @@ import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(IndexController.class)
+@WebMvcTest(controllers = IndexController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, classes= SecurityConfig.class)
+        })
 public class IndexControllerUnitTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private PostsService postsService;
+
     @Test
+    @WithMockUser(roles = "USER")
     public void 메인폐이지_로딩() throws Exception {
         // when
         mvc.perform(MockMvcRequestBuilders.get("/"))
