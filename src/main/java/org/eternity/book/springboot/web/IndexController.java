abc0254ce040a4a476518d8c06cd3b970a25d89e
snatcher93 <snatcher93@hanmail.net>
2020-04-08 13:21:48 +0900

    login 기능 추가

--- src/main/java/org/eternity/book/springboot/web/IndexController.java
+++ src/main/java/org/eternity/book/springboot/web/IndexController.java
@@ -3,19 +3,29 @@ package org.eternity.book.springboot.web;
 import lombok.RequiredArgsConstructor;
 import org.eternity.book.springboot.service.posts.PostsService;
 import org.eternity.book.springboot.web.dto.PostsResponseDto;
+import org.eternity.book.springboot.web.session.SessionUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser)httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
