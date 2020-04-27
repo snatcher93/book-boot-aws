0b688bd57b59fe445c9c8681879672cf4af27e1b
snatcher93 <snatcher93@hanmail.net>
2020-04-27 22:49:01 +0900

    deploy.sh 오타 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=book-boot-aws
 
 echo "> Build 파일 복사"
-cd $REPOSITORY/$PROJECT_NAME/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
